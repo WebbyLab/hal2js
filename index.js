1f3c749627d156a7185cef1513b12552e9fd112e
Alexey Husakov <zoom_@ukr.net>
2016-05-14 19:33:28 +0300

    wc

--- index.js
+++ index.js
@@ -35,12 +35,38 @@ function replaceNode(ast) {
     });
 }
 
+function replaceCallExpression(ast) {
+
+    Object.keys(ast).forEach(key => {
+        if (ast[key].type === 'ExpressionStatement') {
+            if (ast[key].expression.type === 'CallExpression') {
+                const callee = ast[key].expression.callee;
+                ast[key].expression.callee = {type:"MemberExpression",
+                                              object:{
+                                                 type:"Identifier",
+                                                 name:"hal"
+                                              },
+                                              property:callee,
+                                              computed:false
+                                            };
+                if (typeof ast[key] === 'object') {
+                    replaceCallExpression(ast[key]);
+                }                  
+            }
+        } else if (typeof ast[key] === 'object') {
+            replaceCallExpression(ast[key]);
+        }
+    });
+}
+
 replaceNode(ast.program.body);
+replaceCallExpression(ast.program.body);
+console.log(ast.program.body[0].expression);
 const res = babel.transformFromAst(ast);
 
 console.dir(res.code);
 
-/*const code = 'a + "" + b';
+/*const code = 'time.getDay();';
 const ast2 = babylon.parse(code, { allowReturnOutsideFunction: true });
 
 fs.writeFile('./111.json', JSON.stringify(ast2), function(err) {
@@ -48,5 +74,41 @@ fs.writeFile('./111.json', JSON.stringify(ast2), function(err) {
         return console.log(err);
     }
     console.log('Ok');
-});
+});*/
+
+/*{  
+    "type":"ExpressionStatement",
+    "expression":{  
+       "type":"CallExpression",
+       "callee":{  
+          "type":"Identifier",
+          "name":"getDay"
+       },
+       "arguments":[
+
+       ]
+    }
+ }
+
+    "type":"ExpressionStatement",
+    "expression":{  
+       "callee":{  
+          "type":"MemberExpression",
+          "object":{  
+             "type":"Identifier",
+             "name":"time"
+          },
+          "property":{  
+             "type":"Identifier",
+             "name":"getDay"
+          },
+          "computed":false
+       },
+       "arguments":[  
+
+       ]
+    }
+
 */
+
+       
