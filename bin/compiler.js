ad9bdf04926db8a8179451bdda4234a4cd31b5d8
siavolt <siavolt1@gmail.com>
2016-05-14 17:19:31 +0300

    Add compiler script

--- bin/compiler.js
+++ bin/compiler.js
@@ -1,1 +1,31 @@
-// opttions
+#!/usr/bin/env node
+'use strict';
+
+const fs        = require('fs')
+const {docopt}  = require('docopt');
+const {compile} = require('../parser/compiler.js');
+
+const doc = [
+    '',
+    'Usage:',
+    '   compiler.js --src=<src> --dst=<dst>',
+    '   compiler.js -h | --help',
+    '',
+    'Options:',
+    '   -h --help               Show this screen.',
+    '   -s --src <src>          Source HAL code.',
+    '   -d --dst <dst>          Destination JS code.',
+    ''
+].join('\n');
+
+const opts = docopt(doc);
+const src  = opts['--src'];
+const dst  = opts['--dst'];
+
+function main(){
+    const source = fs.readFileSync(src);
+    const ast    = compile(source);
+    return fs.writeFileSync(dst, JSON.stringify(ast));
+}
+
+main();
