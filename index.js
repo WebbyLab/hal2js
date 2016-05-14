34f72d0fa8839143e7e99f7083264d5c988563ed
koorchik <koorchik@gmail.com>
2016-05-14 23:59:37 +0300

    Normzalize identifiers

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const babel = require('babel-core');
 const babylon = require('babylon');
 const fs = require('fs');
 
-const code = 'const hal = require("./hal-runtime");';
+const code = 'let teSt = 12; Test = test + 10; function myFunc() {}; myfunc()';
 const ast2 = babylon.parse(code, { allowReturnOutsideFunction: true });
 
 fs.writeFile('./111.json', JSON.stringify(ast2, null, 4), function(err) {
