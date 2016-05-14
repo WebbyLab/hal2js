fd4c4cf3c6dad486838ea5b19b6940defde5d974
Alexey Husakov <zoom_@ukr.net>
2016-05-14 21:08:55 +0300

    Add ReplaceCallExpression

--- index.js
+++ index.js
@@ -2,71 +2,8 @@
 const babel = require('babel-core');
 const babylon = require('babylon');
 const fs = require('fs');
-const ast = require('./transforms/concat.hal.ast.json');
 
-function replaceNode(ast) {
-
-    Object.keys(ast).forEach(key => {
-        if (ast[key].type === 'BinaryExpression') {
-            if (ast[key].operator === '&') {
-                const left = ast[key].left;
-                const right = ast[key].right;
-                ast[key] = {  
-                    type:"ExpressionStatement",
-                    expression:{  
-                       type:"BinaryExpression",
-                       left:{  
-                          type:"BinaryExpression",
-                          left:left,
-                          operator:"+",
-                          right:{  
-                             type:"StringLiteral",
-                             value:""
-                          }
-                       },
-                       operator:"+",
-                       right:right
-                    }
-                 };
-            }
-        } else if (typeof ast[key].expression === 'object') {
-            replaceNode(ast[key].expression);
-        }
-    });
-}
-
-function replaceCallExpression(ast) {
-
-    Object.keys(ast).forEach(key => {
-        if (ast[key].type === 'ExpressionStatement') {
-            if (ast[key].expression.type === 'CallExpression') {
-                const callee = ast[key].expression.callee;
-                ast[key].expression.callee = {type:"MemberExpression",
-                                              object:{
-                                                 type:"Identifier",
-                                                 name:"hal"
-                                              },
-                                              property:callee,
-                                              computed:false
-                                            };
-                if (typeof ast[key] === 'object') {
-                    replaceCallExpression(ast[key]);
-                }                  
-            }
-        } else if (typeof ast[key] === 'object') {
-            replaceCallExpression(ast[key]);
-        }
-    });
-}
-
-replaceNode(ast.program.body);
-replaceCallExpression(ast.program.body);
-console.log(ast.program.body[0].expression);
-const res = babel.transformFromAst(ast);
-
-console.dir(res.code);
-
-/*const code = 'time.getDay();';
+const code = 'getDay();';
 const ast2 = babylon.parse(code, { allowReturnOutsideFunction: true });
 
 fs.writeFile('./111.json', JSON.stringify(ast2), function(err) {
@@ -74,41 +11,6 @@ fs.writeFile('./111.json', JSON.stringify(ast2), function(err) {
         return console.log(err);
     }
     console.log('Ok');
-});*/
-
-/*{  
-    "type":"ExpressionStatement",
-    "expression":{  
-       "type":"CallExpression",
-       "callee":{  
-          "type":"Identifier",
-          "name":"getDay"
-       },
-       "arguments":[
-
-       ]
-    }
- }
-
-    "type":"ExpressionStatement",
-    "expression":{  
-       "callee":{  
-          "type":"MemberExpression",
-          "object":{  
-             "type":"Identifier",
-             "name":"time"
-          },
-          "property":{  
-             "type":"Identifier",
-             "name":"getDay"
-          },
-          "computed":false
-       },
-       "arguments":[  
-
-       ]
-    }
-
-*/
+});
 
        
