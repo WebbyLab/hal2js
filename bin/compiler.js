b68742c9b5b755e2270fab2a99d9b4854b959a8c
siavolt <siavolt1@gmail.com>
2016-05-14 17:35:51 +0300

    Remove join for docopt options

--- bin/compiler.js
+++ bin/compiler.js
@@ -5,19 +5,16 @@ const fs        = require('fs')
 const {docopt}  = require('docopt');
 const compile   = require('../parser/compiler.js');
 
-const doc = [
-    '',
-    'Usage:',
-    '   compiler.js --src=<src> [--dst=<dst>]',
-    '   compiler.js -h | --help',
-    '',
-    'Options:',
-    '   -h --help               Show this screen.',
-    '   -s --src <src>          Source HAL code.',
-    '   -d --dst <dst>          Destination JS code.',
-    ''
-].join('\n');
-
+const doc =  `
+    Usage:
+       compiler.js --src=<src> [--dst=<dst>]
+       compiler.js -h | --help
+    
+    Options:'
+       -h --help               Show this screen.
+       -s --src <src>          Source HAL code.
+       -d --dst <dst>          Destination JS code.`;
+       
 const opts = docopt(doc);
 const src  = opts['--src'];
 const dst  = opts['--dst'];
