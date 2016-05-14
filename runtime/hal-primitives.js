2cd714714c9b5e3a78d3c6176d41538237b52058
koorchik <koorchik@gmail.com>
2016-05-14 21:39:54 +0300

    Runtime

--- runtime/hal-primitives.js
+++ runtime/hal-primitives.js
@@ -18,6 +18,14 @@ exports.nonblank = function(val) {
 
 };
 
-exports.stopalert = (msg) {
-    console.log(msg);
+exports.getCurTick = function() {
+
+};
+
+exports.stopalert = function(msg) {
+    console.log('stopalert: [%s]', msg);
+};
+
+exports.MessageBox = function(msg) {
+    console.log('MessageBox: [%s]', msg);
 };
