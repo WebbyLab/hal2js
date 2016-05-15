2d823dd0629134c749cd32c8e1281de1b831aeb5
Alexey Husakov <zoom_@ukr.net>
2016-05-15 04:47:58 +0300

    Add async await calls

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const babel = require('babel-core');
 const babylon = require('babylon');
 const fs = require('fs');
 
-const code = 'const hal = require("./hal-runtime");';
-const ast2 = babylon.parse(code, { allowReturnOutsideFunction: true });
+const code = 'function foo(){ let json = getJSON("file.json"); };';
+const ast2 = babylon.parse(code, { allowReturnOutsideFunction: true , plugins: ['asyncFunctions'] });
 
 fs.writeFile('./111.json', JSON.stringify(ast2, null, 4), function(err) {
     if(err) {
