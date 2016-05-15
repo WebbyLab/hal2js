3e38113ca47b9249f1e355f3b9b456d2eed4b0f7
koorchik <koorchik@gmail.com>
2016-05-15 04:59:57 +0300

    Tests for async/await calls

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const babel = require('babel-core');
 const babylon = require('babylon');
 const fs = require('fs');
 
-const code = 'let teSt = 12; Test = test + 10; function myFunc() {}; myfunc()';
+const code = 'function myFunc() { let res = ReadFirstMain(IVr,1,true) };';
 const ast2 = babylon.parse(code, { allowReturnOutsideFunction: true });
 
 fs.writeFile('./111.json', JSON.stringify(ast2, null, 4), function(err) {
